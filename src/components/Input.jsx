0a51b1d03d53d86cfc20d201d0031a056b3445e7
Salwan Arar <salwan.arar@gmail.com>
2025-05-14 13:18:57 +0300

    Refactor Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,17 @@
-const Input = ({ label, id, className = "", ...props }) => {
+const Input = ({ label, id, className = "", trainling=null,...props }) => {
   return (
-    <div>
+    <div className="relative">
       {label && (
-        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        <label htmlFor={id} className="block text-sm font-medium capitalize text-gray-500">
           {label}
         </label>
       )}
       <input
         id={id}
-        className={`mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 ${className}`}
+        className={`w-full px-4 py-2 mt-2 border rounded focus:outline-none focus:ring-2 focus:ring-primary-500 ${className}`}
         {...props}
       />
+      {trainling}
     </div>
   );
 };
