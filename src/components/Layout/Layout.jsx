ea123d52b1d67032045c54d16bc14bba84bbb070
Salwan Arar <salwan.arar@gmail.com>
2025-05-18 10:24:55 +0300

    Add Layout component.

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Sidebar from "./sidebar/Sidebar";
+import Header from "../Layout/Header";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header setSidebarOpen={setSidebarOpen} />
+        <main className="flex-1 overflow-y-auto bg-bg-color p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
