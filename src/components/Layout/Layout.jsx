656a30bc042450e0e60060f44918b4cd4c695c90
Salwan Arar <salwan.arar@gmail.com>
2025-05-20 20:30:28 +0300

    Add ToastNotifier to Layout component for improved user notifications.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Sidebar from "./sidebar/Sidebar";
 import Header from "../Layout/Header";
 import { Outlet } from "react-router-dom";
+import ToastNotifier from "../ToastNotifier";
 
 const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -10,6 +11,7 @@ const Layout = () => {
     <div className="flex h-screen">
       <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
       <div className="flex-1 flex flex-col overflow-hidden">
+        <ToastNotifier />
         <Header setSidebarOpen={setSidebarOpen} />
         <main className="flex-1 overflow-y-auto bg-bg-color p-4">
           <Outlet />
