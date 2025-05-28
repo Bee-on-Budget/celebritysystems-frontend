b7a19b3a6969465a6d07bb0eab331f8e6472d3b4
Salwan Arar <salwan.arar@gmail.com>
2025-05-28 18:29:21 +0300

    Add ScreenService API functions for creating screens, modules, and cabinets

--- src/api/ScreenService.js
+++ src/api/ScreenService.js
@@ -1,9 +1,8 @@
-// ScreenService.js
-import axios from "../../api/axios";
+import api from "./axios";
 
 export const createScreen = async (formData) => {
     try {
-        const response = await axios.post(`/screens`, formData, {
+        const response = await api.post(`/screens`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
@@ -14,18 +13,22 @@ export const createScreen = async (formData) => {
     }
 };
 
-export const createModule = async (moduleData) => {
+export const createModules = async (moduleData) => {
+    console.log("Create Module");
+    console.log(moduleData);
     try {
-        const response = await axios.post(`/module`, moduleData);
+        const response = await api.post(`/module`, moduleData);
         return response.data;
     } catch (error) {
         throw error.response?.data?.message || "Error creating module";
     }
 };
 
-export const createCabin = async (cabinData) => {
+export const createCabinets = async (cabinetData) => {
+    console.log("Create Cabinet");
+    console.log(cabinetData);
     try {
-        const response = await axios.post(`/cabin`, cabinData);
+        const response = await api.post(`/cabin`, cabinetData);
         return response.data;
     } catch (error) {
         throw error.response?.data?.message || "Error creating cabin";
