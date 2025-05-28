a2734ce6a736cc0cc0f00c06d598acf5137ece8e
Salwan Arar <salwan.arar@gmail.com>
2025-05-28 18:36:11 +0300

    Add index file to export multiple components for easier imports

---
+++ src/components/index.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+import Input from "./Input";
+import DropdownInput from "./DropdownInput";
+import FormsContainer from "./FormsContainer";
+import UserForm from "./UserForm";
+import CompanyLogo from "./CompanyLogo";
+import MultiSearchBar from "./MultiSearchBar";
+import ToastNotifier from "./ToastNotifier";
+import PasswordStrengthMeter from "./PasswordStrengthMeter";
+import NavButton from "./NavButton";
+import Loading from "./Loading";
+
+export {
+  Button,
+  Input,
+  DropdownInput,
+  FormsContainer,
+  UserForm,
+  CompanyLogo,
+  MultiSearchBar,
+  ToastNotifier,
+  PasswordStrengthMeter,
+  NavButton,
+  Loading
+};
