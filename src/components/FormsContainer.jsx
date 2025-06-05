719221e8f795c676facfa28dbcb8fd7c6a7b69e9
Salwan Arar <salwan.arar@gmail.com>
2025-06-05 09:21:43 +0300

    Update FormsContainer component to add top margin.

--- src/components/FormsContainer.jsx
+++ src/components/FormsContainer.jsx
@@ -1,6 +1,6 @@
 const FormsContainer = ({children}) => {
     return (
-        <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-8 border border-gray-100">
+        <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-8 border border-gray-100 mt-8">
             {children}
         </div>
     );
