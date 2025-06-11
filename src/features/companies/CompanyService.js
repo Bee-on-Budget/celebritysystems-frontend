bd27420503f132e30ab400e7490bdc5f63c3a9e9
Salwan Arar <salwan.arar@gmail.com>
2025-06-12 02:27:50 +0300

    Refactor CompanyService to use 'api' alias instead of 'axios' for improved clarity and consistency in API calls.

--- src/features/companies/CompanyService.js
+++ src/features/companies/CompanyService.js
@@ -1,8 +1,7 @@
-// src/features/companies/CompanyService.js
-import axios from "../../api/axios";
+import api from "../../api/axios";
 
-export const createCompany = (data) => axios.post("/company", data);
-export const getAllCompanies = () => axios.get("/company");
-export const deleteCompany = (id) => axios.delete(`/company/${id}`);
-export const getCompanyById = (id) => axios.get(`/company/id/${id}`);
-export const getCompanyByName = (name) => axios.get(`/company/name/${name}`);
+export const createCompany = (data) => api.post("/company", data);
+export const getAllCompanies = () => api.get("/company");
+export const deleteCompany = (id) => api.delete(`/company/${id}`);
+export const getCompanyById = (id) => api.get(`/company/id/${id}`);
+export const getCompanyByName = (name) => api.get(`/company/name/${name}`);
