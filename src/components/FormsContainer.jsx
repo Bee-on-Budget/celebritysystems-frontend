c7291d293de5c670daaee59b98217ca3e3e793bc
Salwan Arar <salwan.arar@gmail.com>
2025-06-25 22:41:51 +0300

    Enhance FormsContainer component by adding title, action button, and form handling for improved user interaction and layout.

--- src/components/FormsContainer.jsx
+++ src/components/FormsContainer.jsx
@@ -1,7 +1,17 @@
-const FormsContainer = ({children}) => {
+import { Button } from "./";
+
+const FormsContainer = ({ title, actionTitle, isLoading, onSubmit, children }) => {
     return (
         <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-8 border border-gray-100 mt-8">
-            {children}
+            <h1 className="text-2xl font-semibold mb-8">{title}</h1>
+            <form onSubmit={onSubmit}>
+                <div className={"grid grid-cols-1 md:grid-cols-2 gap-6"}>
+                {children}
+                </div>
+                <div className="mt-8 pt-6 border-t border-gray-200">
+                    <Button isLoading={isLoading} type="submit">{actionTitle}</Button>
+                </div>
+            </form>
         </div>
     );
 };
