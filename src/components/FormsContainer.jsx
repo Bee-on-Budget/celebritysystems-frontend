725e30b50754101ff0c6c707b88d2df5e499d89c
Salwan Arar <salwan.arar@gmail.com>
2025-06-26 20:36:07 +0300

    Update FormsContainer component to include noValidate attribute in the form element, improving form validation handling.

--- src/components/FormsContainer.jsx
+++ src/components/FormsContainer.jsx
@@ -4,7 +4,7 @@ const FormsContainer = ({ title, actionTitle, isLoading, onSubmit, children }) =
     return (
         <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-8 border border-gray-100 mt-8">
             <h1 className="text-2xl font-semibold mb-8">{title}</h1>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} noValidate>
                 <div className={"grid grid-cols-1 md:grid-cols-2 gap-6"}>
                 {children}
                 </div>
